0c5d9b228d03ef5134b308beeb57ccb14b259693
Dushyant Bhambore <dushyantbhambore@sdeichd.com>
2024-12-16 13:54:17 +0530

    merge conflict fix

---
+++ Backend/EHRPortal/App.Core/Dto/GetAllProviderBySpecialisationIdDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Dto
+{
+    public class GetAllProviderBySpecialisationIdDto
+    {
+        public int ProviderId { get; set; }
+        public int SpecialisationId { get; set; }
+    }
+}
