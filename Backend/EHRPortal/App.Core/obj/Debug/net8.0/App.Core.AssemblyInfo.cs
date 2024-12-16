931da75af8f536fb65e2d57e0a31294467e72da3
DushyantBhambore <goodbyebeautiful07@gmail.com>
2024-12-16 23:23:14 +0530

    update

--- Backend/EHRPortal/App.Core/obj/Debug/net8.0/App.Core.AssemblyInfo.cs
+++ Backend/EHRPortal/App.Core/obj/Debug/net8.0/App.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08614174b95dd833a230421936cd5e85cdce062f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a21555f0c6ed4e5a14345105e1122c826372a88")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
