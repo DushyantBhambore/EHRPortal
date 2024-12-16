0c8d4b08324eb3d49349a6fce88f0f492ebbbdcc
DushyantBhambore <goodbyebeautiful07@gmail.com>
2024-12-16 23:23:28 +0530

    update

--- Backend/EHRPortal/EHRPortal/obj/Debug/net8.0/EHRPortal.AssemblyInfo.cs
+++ Backend/EHRPortal/EHRPortal/obj/Debug/net8.0/EHRPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EHRPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a21555f0c6ed4e5a14345105e1122c826372a88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931da75af8f536fb65e2d57e0a31294467e72da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EHRPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EHRPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
